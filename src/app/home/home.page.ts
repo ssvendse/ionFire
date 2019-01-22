e3df6ee2a70081b072e44739aa5804122e2280a9
Skyler Svendsen <skylersvendsen@gmail.com>
2019-01-22 15:40:44 -0600

    added button to view tutorial again (not in lesson plan)

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Storage } from '@ionic/storage';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +8,9 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
+  constructor(private storage: Storage, private router: Router) { }
+  async revertTutorial() {
+    await this.storage.set('tutorialComplete', false);
+    this.router.navigateByUrl('/tutorial');
+  }
 }
