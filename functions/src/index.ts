e97de94bf5bd0ae83f3135e4e44daaea195bc3d7
Skyler Svendsen <skylersvendsen@gmail.com>
2019-01-24 11:23:56 -0600

    firebase cloud messaging backend functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,8 @@
-import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
+admin.initializeApp();
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+export {
+    subscribeToTopic,
+    unsubscribeFromTopic,
+    sendOnFirestoreCreate
+} from './fcm';
